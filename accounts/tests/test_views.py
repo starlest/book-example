35e6219ea680ccc3c8f125b2273133a813beaf6f
Edwin Chia <starlest@gmail.com>
2017-12-12 10:31:22 +0700

    Custom passwordless auth backend + custom user model

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -34,13 +34,6 @@ class SendLoginEmailViewTest(TestCase):
         expected = "Check your email, we've sent you a link you can use to log in."
         self.assertEqual(mock_messages.success.call_args, call(response.wsgi_request, expected))
 
-
-class LoginViewTest(TestCase):
-
-    def test_redirects_to_home_page(self):
-        response = self.client.get('/accounts/login?token=abcd123')
-        self.assertRedirects(response, '/')
-
     def test_creates_token_associated_with_email(self):
         self.client.post('/accounts/send_login_email', data={
             'email': 'starlest@gmail.com'
@@ -58,3 +51,27 @@ class LoginViewTest(TestCase):
         expected_url = f'http://testserver/accounts/login?token={token.uid}'
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertIn(expected_url, body)
+
+
+@patch('accounts.views.auth')
+class LoginViewTest(TestCase):
+
+    def test_redirects_to_home_page(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
+
+    def test_calls_authenticate_with_uid_from_request(self, mock_auth):
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.authenticate.call_args, call(uid='abcd123'))
+
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.login.called, False)
