4d52a2145eca6cae57ad2fa3bfdda8eb29cfe78d
Edwin Chia <starlest@hotmail.com>
2017-12-14 12:59:59 +0700

    fix bug

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -8,12 +8,12 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    with settings(host_string=f'starlest@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    with settings(host_string=f'starlest@{host}'):
         session_key = run(f'{manage_dot_py} create_session {email}')
         return session_key.strip()
