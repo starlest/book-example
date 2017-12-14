702667462289e50bb7ca964199066a1dd2811cca
Edwin Chia <starlest@gmail.com>
2017-12-14 18:58:24 +0700

    test

--- superlists/settings.py
+++ superlists/settings.py
@@ -142,7 +142,7 @@ LOGGING = {
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'support@bigetron.gg'
+EMAIL_HOST_USER = 'starlest@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
