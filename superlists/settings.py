1ec0a32dcd3670c15830299034a7222d3cdf04e2
Edwin Chia <starlest@gmail.com>
2017-12-14 19:26:52 +0700

    hopeless

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,7 +132,7 @@ LOGGING = {
         'logfile': {
             'level':'DEBUG',
             'class':'logging.FileHandler',
-            'filename': BASE_DIR + "../../error.log",
+            'filename': BASE_DIR + "/../../error.log",
         },
     },
     'root': {
